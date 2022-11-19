34bd8b6addf031b1e92df9a924498e145b0d1618
Nikita Alekseev <niktest@mail.ru>
2022-11-20 09:27:13 +1000

    Fixing MySQL installation

--- install/hst-install-ubuntu.sh
+++ install/hst-install-ubuntu.sh
@@ -34,7 +34,7 @@ VERBOSE='no'
 HESTIA_INSTALL_VER='1.6.11'
 # Dependencies
 multiphp_v=("5.6" "7.0" "7.1" "7.2" "7.3" "7.4" "8.0" "8.1")
-fpm_v="8.0"
+fpm_v="8.1"
 mariadb_v="10.6"
 
 # Defining software pack for all distros
